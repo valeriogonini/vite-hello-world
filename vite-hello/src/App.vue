bb2f64542d142ed9cfaf3d870b3d640a7eb1e903
valeriogonini <valeriogonini@gmail.com>
2024-04-09 17:25:53 +0200

    add apptitle

---
+++ vite-hello/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
+export default {
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
+
+</script>
+
+
+<template>
+ 
+    <AppTitle />
+    <AppHero />
+
+</template>
+
---
+++ vite-hello/src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+export default{
+    data(){
+        return{
+            message: "La mia prima App con VITE"
+        }
+    }
+}
+</script>
+<template>
+<div id="AppTitle">
+    <h1 class="center">
+        {{ message }}
+    </h1>
+</div>
+</template>
+<style>
+
+</style>
